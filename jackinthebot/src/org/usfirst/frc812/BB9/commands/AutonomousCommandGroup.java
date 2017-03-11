285979439e200e24e76143a0ba31030112a19c4f
FRC812 <frc@team812.org>
2017-03-11 09:35:08 -0800

    minor switch refactor left | straight | right

--- jackinthebot/src/org/usfirst/frc812/BB9/commands/AutonomousCommandGroup.java
+++ jackinthebot/src/org/usfirst/frc812/BB9/commands/AutonomousCommandGroup.java
@@ -13,16 +13,16 @@ public AutonomousCommandGroup() {
 	
 	System.out.println("I am in AutonomousCommandGroup");
 	
-    if( Robot.controlBoxSubsystem.isSwitchLeft() ) {
+    if( Robot.controlBoxSubsystem.isSwitchCenter() ) {
     	System.out.println("switch is left, speed 0.5, curve 0, time 3 sec");
-    	addSequential(new DriveByTime(0.5, 0, 3.0) );
+    	addSequential(new DriveByTime(0.5, 0, 4.0) );
 							
-    } else if ( Robot.controlBoxSubsystem.isSwitchCenter() ) {
+    } else if ( Robot.controlBoxSubsystem.isSwitchRight() ) {
     	System.out.println("switch is center, speed 0.5, curve 1, time 1 sec");
     	addSequential(new DriveByTime(0.5, 0, 3.0) );
     	addSequential(new DriveByTime(0.5, 1, 1.0) );
 			
-    } else if( Robot.controlBoxSubsystem.isSwitchRight() ){
+    } else if( Robot.controlBoxSubsystem.isSwitchLeft() ){
     	System.out.println("switch is right, speed 0.5, curve -1, time 1 sec");
     	addSequential(new DriveByTime(0.5,  0, 3.0) );
     	addSequential(new DriveByTime(0.5, -1, 1.0) );
---
+++ readme.md
@@ -0,0 +1,7 @@
+== How to check in the code ==
+
+1. Open gitshell
+2. cd ~/workspace/FRC_2017
+3. git add -A
+4. git commit -m 'your commit message saying what you changed'
+5. git push
